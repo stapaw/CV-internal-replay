93f3ba6ca6600f7baeb6a059a831ee8d1a97b70e
Filip <fszatkowski96@gmail.com>
2022-05-31 21:33:47 +0200

    Add FMNIST script

---
+++ scripts/run_fmnist.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+./main_cl.py \
+  --experiment=fmnist \
+  --scenario=class \
+  --tasks 5 \
+  --batch 128 \
+  --seed 0 \
+  --replay=generative \
+  --prior=GMM \
+  --per-class \
+  --g-z-dim 10 \
+  --sample-n 30 \
+  --pdf \
+  --depth=3 \
+  --reducing-layers=2 \
+  --channels=16 \
+  --fc-layers=2 \
+  --fc-units 100 \
+  --gen-iters=2000 \
+  --conv-bn=False \
+  --distill \
+  --freeze-fcE \
+  --freeze-fcE-layer=2
+
+./main_cl.py \
+  --experiment=fmnist \
+  --scenario=class \
+  --tasks 5 \
+  --batch 128 \
+  --seed 0 \
+  --replay=generative \
+  --prior=GMM \
+  --per-class \
+  --g-z-dim 10 \
+  --sample-n 30 \
+  --pdf \
+  --depth=3 \
+  --reducing-layers=2 \
+  --channels=16 \
+  --fc-layers=2 \
+  --fc-units 100 \
+  --gen-iters=2000 \
+  --conv-bn=False \
+  --distill \
+  --freeze-fcE \
+  --freeze-fcE-layer=1
+
+./main_cl.py \
+  --experiment=fmnist \
+  --scenario=class \
+  --tasks 5 \
+  --batch 128 \
+  --seed 0 \
+  --replay=generative \
+  --prior=GMM \
+  --per-class \
+  --g-z-dim 10 \
+  --sample-n 30 \
+  --pdf \
+  --depth=3 \
+  --reducing-layers=2 \
+  --channels=16 \
+  --fc-layers=2 \
+  --fc-units 100 \
+  --gen-iters=2000 \
+  --conv-bn=False \
+  --distill
