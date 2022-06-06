c48f7b728803c182eec59b305ee50c90d0f89677
Michal Bortkiewicz <michalbortkiewicz8@gmail.com>
2022-06-06 12:05:42 +0200

    Minor fix

--- scripts/fmnist_grid_search.sh
+++ scripts/fmnist_grid_search.sh
@@ -7,14 +7,37 @@ MODEL_TAG=cifar10_${FC_LAYERS}fc_${HIDDEN_NEURONS}hn_proper
 BATCH_REPLAY=256
 ITERS=5000
 
-./main_pretrain.py \
-  --experiment=CIFAR10 \
-  --epochs=100 \
-  --augment
-  --fc-units=${HIDDEN_NEURONS} \
-  --fc-layers=${FC_LAYERS} \
-  --convE-stag=${MODEL_TAG}
+#./main_pretrain.py \
+#  --experiment=CIFAR10 \
+#  --epochs=100 \
+#  --augment
+#  --fc-units=${HIDDEN_NEURONS} \
+#  --fc-layers=${FC_LAYERS} \
+#  --convE-stag=${MODEL_TAG}
 
+# Brain inspired sanity check
+#./main_cl.py \
+#  --experiment=CIFAR100 \
+#  --scenario=class \
+#  --tasks=10 \
+#  --batch=256 \
+#  --batch-replay=${BATCH_REPLAY} \
+#  --seed=0 \
+#  --replay=generative \
+#  --prior=GMM \
+#  --per-class \
+#  --g-z-dim=100 \
+#  --sample-n=30 \
+#  --pdf \
+#  --depth=5 \
+#  --fc-layers=${FC_LAYERS} \
+#  --fc-units=${HIDDEN_NEURONS} \
+#  --iters=${ITERS} \
+#  --distill \
+#  --freeze-convE \
+#  --brain-inspired \
+#  --pre-convE \
+#  --convE-ltag=${MODEL_TAG} \
 
 FC_LATENT_LAYER=0
 ./main_cl.py \
